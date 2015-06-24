1b0573455937707dd73c30cd87eeecffc5cbee98
Luis Madrigal <luis.felipe@ooyala.com>
2015-06-24 08:56:31 -0500

    Style: apply jshint suggestions

--- anarchify.js
+++ anarchify.js
@@ -1,9 +1,9 @@
 function anarchifyImage(info, tab) {
-  var url = info.srcUrl
+  var url = info.srcUrl;
   if (info.mediaType == "video") {
     // Services such as imgur accept changing the extension from "webm" to "gif"
     if (!canConvertToGif(url)) {
-      console.log("Can't convert video",url,"to gif")
+      console.log("Can't convert video",url,"to gif");
       return;
     }
     url = rewriteExtensionToGif(url);
@@ -14,7 +14,7 @@ function anarchifyImage(info, tab) {
 function postToAnarchy (url) {
   var destination = localStorage.anarchyUrl;
 
-  if (destination != undefined && destination != null) {
+  if (destination !== undefined && destination !== null) {
     var r = new XMLHttpRequest();
     r.open("POST", destination, true);
     r.onreadystatechange = function () {
@@ -28,17 +28,17 @@ function postToAnarchy (url) {
 }
 
 function canConvertToGif(url) {
-  var patterns = ["imgur.com", "giphy.com"]
+  var patterns = ["imgur.com", "giphy.com"];
   for (var i = 0; i < patterns.length; i++) {
     if (url.includes(patterns[i])) {
       return true;
     }
-  };
-  return false
+  }
+  return false;
 }
 
 function rewriteExtensionToGif(url) {
-  return url.substr(0, url.lastIndexOf(".")) + ".gif"
+  return url.substr(0, url.lastIndexOf(".")) + ".gif";
 }
 
 chrome.contextMenus.create({
